e7d29478a4f44b78719a9e626a4b83986703c1d2
jeny797 <jeny797@gmail.com>
2021-03-21 22:03:36 +0200

    added errors handler

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,16 +6,21 @@ from .models import FeedItem, RegisteredUser
 class FeedItemListView(ListView):
     model = FeedItem
     template_name = 'home.html'
+    errors = []
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         active_user = self.request.user
         filter_parametr = self.request.GET.get('answer')
         if filter_parametr == 'my_posts' or filter_parametr == 'my_posts_subscribe':
-            registered_user = RegisteredUser.objects.get(user=active_user)
-            tracking_list = [active_user.id]
-            if filter_parametr == 'my_posts_subscribe':
-                tracking_list += [item.user.id for item in RegisteredUser.objects.get(user=active_user).tracking.all()]
-            context['object_list'] = [item for item in FeedItem.objects.all() if item.user.id in tracking_list]
+            try:
+                registered_user = RegisteredUser.objects.get(user=active_user)
+                tracking_list = [active_user.id]
+                if filter_parametr == 'my_posts_subscribe':
+                    tracking_list += [item.user.id for item in RegisteredUser.objects.get(user=active_user).tracking.all()]
+                context['object_list'] = [item for item in FeedItem.objects.all() if item.user.id in tracking_list]
+            except:
+                errors = 'Please, register for sorting data'
+                context['errors'] = errors
 
         return context
